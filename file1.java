2fe35c2991dcc9dbf1453793db152a8c1ce50c6c
bhaskar <bhaskar@server2.abc.com>
2023-04-28 16:05:48 +0000

    file1 by bhaskar

---
+++ file1.java
@@ -0,0 +1,9 @@
+package demo
+class Hello {
+ public static void main(String args[]){
+	 int a=5;
+	 String name= new String("Swetha");
+	 String name2="Bhaskar";
+  System.out.println("Hello world");
+ }
+}
